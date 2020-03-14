6dc67827061d05c98d85aac42e9ee4b40a173e4e
Elias de Francisco <elias@iMac-de-Elias.local>
2020-03-14 21:10:53 +0100

    using 404 template

--- src/app.js
+++ src/app.js
@@ -19,10 +19,10 @@ app.use(require('./routes/entries.routes'))
 
 // 404 handler
 app.use((req, res) => {
-    res.status(404).send('404 not found')
+    res.status(404).render('404')
 })
 
 // Starting the app
 app.listen(app.get('port'), () => {
-    console.log('Server on port ', app.get('port'))
+    console.log('Server on port:', app.get('port'))
 })
